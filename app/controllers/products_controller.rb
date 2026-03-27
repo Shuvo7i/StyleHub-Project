2acf01e7b46aaf33a50da013e2f65cefdc04c5a0
Shuvo7i <akramulhoque1050@gmail.com>
2026-03-27 03:06:24 -0500

    Created on sale, recently update etc

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,7 +1,20 @@
 class ProductsController < ApplicationController
     def index
+        @filter = params[:filter]
         @products = Product.includes(:category).order(created_at: :desc)
+
+    case @filter
+    when "on_sale"
+      @products = @products.where(on_sale: true)
+    when "new"
+      @products = @products.where("created_at >= ?", 3.days.ago)
+    when "recently_updated"
+      @products = @products.where("updated_at >= ?", 3.days.ago)
+                           .where("created_at < ?", 3.days.ago)
+                           .order(updated_at: :desc)
     end
+  end
+
     def show
         @product = Product.find(params[:id])
     end
