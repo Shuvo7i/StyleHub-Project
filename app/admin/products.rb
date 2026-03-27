ecd96dcd252434f4d5a57fa56555fa163265b178
Shuvo7i <akramulhoque1050@gmail.com>
2026-03-26 22:31:07 -0500

    Did a image field and show image

--- app/admin/products.rb
+++ app/admin/products.rb
@@ -1,6 +1,6 @@
 ActiveAdmin.register Product do
   permit_params :category_id, :name, :description, :sku, :price, :stock_quantity,
-                :size, :color, :material, :on_sale, :featured
+                :size, :color, :material, :on_sale, :featured, :image
 
   index do
     selectable_column
@@ -34,7 +34,20 @@ ActiveAdmin.register Product do
       f.input :material
       f.input :on_sale
       f.input :featured
+      f.input :image, as: :file
     end
     f.actions
   end
+show do
+  attributes_table do
+    row :color
+    row :image do |product|
+      if product.image.attached?
+        image_tag url_for(product.image), width: 120
+      else
+        "No image uploaded"
+      end
+    end
+  end
+end
 end
--- app/models/product.rb
+++ app/models/product.rb
@@ -1,6 +1,7 @@
 class Product < ApplicationRecord
   belongs_to :category
-
+  has_one_attached :image
+  
   validates :name, presence: true
   validates :sku, presence: true, uniqueness: true
   validates :price, presence: true, numericality: { greater_than_or_equal_to: 0 }
