bcd896e55b35e468285c7fc8dfbdee63b2003f43
Shuvo7i <akramulhoque1050@gmail.com>
2026-03-27 03:44:28 -0500

    Created a search for Categories

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -11,9 +11,24 @@ class ProductsController < ApplicationController
       @products = @products.where("created_at >= ?", 3.days.ago)
     when "recently_updated"
       @products = @products.where("updated_at >= ?", 3.days.ago)
-                           .where("createrails consoled_at < ?", 3.days.ago)
+                           .where("created_at < ?", 3.days.ago)
                            .order(updated_at: :desc)
+    else
+      @products = @products.order(created_at: :desc)
     end
+    if params[:q].present?
+      keyword = "%#{params[:q].strip.downcase}%"
+      @products = @products.where(
+        "LOWER(products.name) LIKE ? OR LOWER(products.description) LIKE ?",
+        keyword,
+        keyword
+      )
+  end
+  if params[:category_id].present?
+      @products = @products.where(category_id: params[:category_id])
+    end
+
+    @products = @products.page(params[:page]).per(12)
   end
 
     def show
